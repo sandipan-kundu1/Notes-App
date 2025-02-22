27a5019fed74cf796de6d794666f9e4994d1c06e
Sandipan <kundu.sandipan18@gmail.com>
2025-02-22 17:32:44 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import NoteList from './NoteList';
 import NoteForm from './NoteForm';
-import './index.css';
 
 function App() {
   const [notes, setNotes] = useState([]);
